8c5d38d93e25cdbf64922e390d7baf81994e3e48
lebogang-cloud <you@example.com>
2020-05-29 00:05:56 +0200

    server user routes

--- server/server.js
+++ server/server.js
@@ -41,6 +41,25 @@ app.get("/api/books", (req, res) => {
     });
 });
 
+app.get('/api/getReviewer', (req,res)=>{
+  let id = req.query.id;
+
+  User.findById(id,(err,doc)=>{
+    if(err) return res.status(400).send(err);
+    res.json({
+      name: doc.name,
+      lastname:doc.lastname
+    })
+  })
+})
+
+app.get('/api/users', (req,res)=>{
+  User.find({},(err, users)=>{
+    if(err) return res.status(400).send(err);
+    res.status(200).send(users)
+  })
+})
+
 // POST //
 app.post("/api/book", (req, res) => {
   const book = new Book(req.body);
@@ -53,6 +72,45 @@ app.post("/api/book", (req, res) => {
     });
   });
 });
+
+// User register
+app.post('/api/register', (req, res)=>{
+  const user = new User(req.body);
+  
+  user.save((err,doc)=>{
+    if (err) return res.json({success:false});
+    res.status(200).json({
+      success:true,
+      user:doc
+    })
+  })
+})
+
+//login
+
+app.post('/api/login',(req,res)=>{
+  
+  User.findOne({'email':req.body.email},(err, user)=>{
+      if(!user) return res.json({isAuth:false,message:'Auth failed, email is not vaild'})
+
+    user.comparePassword(req.body.password,(err,isMatch)=>{
+      if(!isMatch) return res.json({
+        isAuth:false,
+        message:'Wrong password'
+      });
+
+      user.generateToken((err,user)=>{
+        if(err) return res.status(400).send(err);
+        res.cookie('auth',user.token).json({
+          isAuth:true,
+          id:user._id,
+          email:user.email
+        })
+      })
+    })
+  })
+})
+
 // UPDATE //
 app.put("/api/book_update", (req, res) => {
   Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
