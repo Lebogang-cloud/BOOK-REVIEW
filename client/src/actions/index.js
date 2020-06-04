d9fa66e5cdc4c9baf5ce314528bc89e067ea3ee3
lebogang-cloud <you@example.com>
2020-06-05 01:57:12 +0200

    create login screen

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
-export function getBooks(
-    limit = 10, 
-    start = 0, 
-    order = "asc", 
-    list = ""
-){
-
+export function getBooks(limit = 10, start = 0, order = "asc", list = "") {
   const request = axios
     .get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
     .then((response) => {
       if (list) {
-        return [...list,...response.data];
+        return [...list, ...response.data];
       } else {
         return response.data;
       }
@@ -23,28 +17,46 @@ export function getBooks(
   };
 }
 
-export function getBookWithReviewer(id){
-    const request = axios.get(`/api/getBook?id=${id}`)
+export function getBookWithReviewer(id) {
+  const request = axios.get(`/api/getBook?id=${id}`);
 
-    return (dispatch)=>{
-        request.then(({data})=>{
-            let book = data;
-            
-            axios.get(`/api/getReviewer?id=${book.ownerId}`)
-            .then(({data})=>{
-                let response ={
-                    book,
-                    reviewer:data
-                }
-console.log(response)
-            
-                dispatch({
-                    type:'GET_BOOK_W_REVIEWER',
-                    payload:response   
+  return (dispatch) => {
+    request.then(({ data }) => {
+      let book = data;
 
-            })
+      axios.get(`/api/getReviewer?id=${book.ownerId}`).then(({ data }) => {
+        let response = {
+          book,
+          reviewer: data,
+        };
+        console.log(response);
+        dispatch({
+          type: "GET_BOOK_W_REVIEWER",
+          payload: response,
+        });
+      });
+    });
+  };
+}
 
-            })
-        })
+export function clearBookWithReviewer(){
+  return {
+    type:'CLEAR_BOOK_W_REVIEWER',
+    payload:{
+      book:{},
+      reviewer:{}
     }
+  }
+}
+
+// ==========USER==========
+
+export function loginUser({email,password}){
+    const request = axios.post('/api/login',{email,password})
+      .then(response =>response.data)
+
+  return {
+    type: 'USER_LOGIN',
+    payload:request
+  }
 }
