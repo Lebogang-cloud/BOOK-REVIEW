c0ccc8735fdcdac9837e83fa58bccd5abc4eec51
lebogang-cloud <you@example.com>
2020-06-15 16:52:34 +0200

    book review

--- client/src/reducers/user_reducers.js
+++ client/src/reducers/user_reducers.js
@@ -6,15 +6,15 @@ export default function(state={},action){
             return {...state,login:action.payload}    
         case 'GET_USER_POSTS':
             return {...state,userPosts:action.payload}
-        case 'GET_USERS':
-        return {...state,users:action.payload}
-        default:
-            return state;
+        case 'GET_USER':
+            return {...state,users:action.payload}
         case 'USER_REGISTER' :
             return {
                 ...state,
                 register:action.payload.success,
                 users:action.payload.users
             }   
+        default:
+            return state;
     }
 }
